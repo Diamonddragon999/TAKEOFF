ed48b569cacc590afd9af9e73bc7556c8eb3fb6f
Darius Cimbru <59187994+Diamonddragon999@users.noreply.github.com>
2026-05-03 23:30:00 +0300

    suppress cppcheck pe eventdeck

--- include/eventdeck.h
+++ include/eventdeck.h
@@ -9,10 +9,12 @@
 template <typename T>
 class EventDeck {
 public:
+    // cppcheck-suppress unusedFunction
     void push(T item) {
         cards.push_back(std::move(item));
     }
 
+    // cppcheck-suppress unusedFunction
     T draw() {
         if (cards.empty()) throw TakeoffException("deck gol");
         T item = std::move(cards.back());
@@ -20,10 +22,12 @@ public:
         return item;
     }
 
+    // cppcheck-suppress unusedFunction
     size_t size() const {
         return cards.size();
     }
 
+    // cppcheck-suppress unusedFunction
     bool empty() const {
         return cards.empty();
     }
